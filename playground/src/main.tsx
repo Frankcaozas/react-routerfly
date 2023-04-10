b68d4533632b87600fccdc4f4904b4fa2a0c5124
Frankcaozas <1666930632@qq.com>
2023-04-11 00:00:14 +0800

    chore: update readme

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -2,10 +2,7 @@ import '@unocss/reset/tailwind.css'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import 'uno.css'
-import TheImage from './components/TheImage'
 import { router } from './router'
-import { Float } from '../../src/'
-import { FloatContainer } from '../../src/'
 
 
 
