0d7737d5d32654e72658db26fd804253a3e2220a
frankcao <1666930632@qq.com>
2023-07-17 16:28:23 +0800

    fix: playground img

--- playground/src/data.ts
+++ playground/src/data.ts
@@ -1,5 +1,30 @@
-export const images = new Array(12).fill('').map((val, idx)=> `../imgs/${idx+1}.jpg`)
-console.log(images)
+import img1 from '../imgs/1.jpg'
+import img10 from '../imgs/10.jpg'
+import img11 from '../imgs/11.jpg'
+import img12 from '../imgs/12.jpg'
+import img2 from '../imgs/2.jpg'
+import img3 from '../imgs/3.jpg'
+import img4 from '../imgs/4.jpg'
+import img5 from '../imgs/5.jpg'
+import img6 from '../imgs/6.jpg'
+import img7 from '../imgs/7.jpg'
+import img8 from '../imgs/8.jpg'
+import img9 from '../imgs/9.jpg'
+export const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+]
+// console.log(images)
 // [
 //   'https://cdn.pixabay.com/photo/2023/03/26/19/57/mountain-7879029_1280.jpg',
 //   'https://cdn.pixabay.com/photo/2023/04/22/19/19/flowers-7944288_1280.jpg',
